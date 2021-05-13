216783e2be2d592e8d2de4189d97e96f57d3b525
Schneur <schneuryiftach@gmail.com>
2021-05-13 15:46:47 -0400

    updated with winner as a alert

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var playersAnswer = prompt('Welcome to the most awesome game of rock, paper scissor!\nchoose either "rock", "paper", or "scissor"', 'write answer here');
-console.log(playersAnswer);
+var welcome = prompt('Welcome to the most awesome game of rock, paper scissor!\nchoose between 1. single round mode. or 2. best out of 3.', 'type 1 or 2');
+console.log(welcome);
+
+var playersAnswer = prompt('great! time to get started!\nchoose either "rock", "paper", or "scissor"', 'write answer here');
 
 var botResult = "";
+
+
 
 var botsAnswer = function () {
   var compAnswer =  Math.floor(Math.random() * 100);
@@ -16,13 +20,16 @@ var botsAnswer = function () {
 var determineWinner = function () {
   console.log(botsAnswer());
   if (botResult === 'bot chooses scissors' && playersAnswer === "paper") {
-    return 'bot wins'} else if (botResult === 'bot chooses paper' && playersAnswer === "rock") {
-      return 'bot wins'} else if (botResult === 'bot chooses rock' && playersAnswer === "scissor") {
-        return 'bot wins'} else if (playersAnswer === "scissor" && botResult === 'bot chooses paper') {
-          return 'you win'} else if ( playersAnswer === "paper" && botResult === "bot chooses rock") {
-            return 'you win'} else if (playersAnswer === "rock" && botResult === 'bot chooses scissors') {
-              return 'you win'} else {
-                return "its a tie"};
+    return alert('bot chose scissors, bot wins')} else if (botResult === 'bot chooses paper' && playersAnswer === "rock") {
+      return alert('bot chose paper, bot wins')} else if (botResult === 'bot chooses rock' && playersAnswer === "scissor") {
+        return alert('bot chose rock, bot wins')} else if (playersAnswer === "scissor" && botResult === 'bot chooses paper') {
+          return alert('bot chose paper, you win')} else if ( playersAnswer === "paper" && botResult === "bot chooses rock") {
+            return alert('bot chose rock, you win')} else if (playersAnswer === "rock" && botResult === 'bot chooses scissors') {
+              return alert('bot chose scissors, you win')} else {
+                return alert("its a draw")};
 };
 
-console.log(determineWinner());
+if (welcome === '1') {
+  console.log(playersAnswer);
+  console.log(determineWinner());
+  };
